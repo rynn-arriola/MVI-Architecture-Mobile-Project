f94d2249b11b966d2508694a8b99cb43dd30965d
rynn-arriola <rynn.arriola@yahoo.com>
2025-02-27 15:40:38 -0500

    create project

---
+++ app/src/main/java/com/example/rynnarriola/mobileproject_mvi/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.rynnarriola.mobileproject_mvi
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+    }
+}
+
